6f134a6e684fd63d080b47fc806c8f6a3025c789
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-08-20 22:28:57 +0800

    Renamed carousel to carousel_oldgit status carousel_old.php

---
+++ carousel_old.php
@@ -0,0 +1,109 @@
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+        <li data-target="#myCarousel" data-slide-to="4"></li>
+        <li data-target="#myCarousel" data-slide-to="5"></li>
+        <li data-target="#myCarousel" data-slide-to="6"></li>
+        <li data-target="#myCarousel" data-slide-to="7"></li>
+        <li data-target="#myCarousel" data-slide-to="8"></li>
+        <li data-target="#myCarousel" data-slide-to="9"></li>
+
+
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+        <div class="item active">
+            <img class="img-responsive center-block" src="gallery/banner1.jpg" >
+            <div class="carousel-caption">
+                <!-- <h3>Banner1</h3>
+                <p>Hebat</p> -->
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="img-responsive center-block" src="gallery/banner2.jpg" >
+            <div class="carousel-caption">
+                <!-- <h3>Banner2</h3>
+                <p>Friends!</p> -->
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="img-responsive center-block" src="gallery/banner3.jpg" >
+            <div class="carousel-caption">
+                <!-- <h3>Banner3</h3>
+                <p>We love Bloomberg 2016</p> -->
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="img-responsive center-block" src="gallery/banner4.jpg" >
+            <div class="carousel-caption">
+                <!-- <h3>Banner4</h3>
+                <p>We love Bloomberg 2016</p> -->
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="img-responsive center-block" src="gallery/banner5.jpg" >
+            <div class="carousel-caption">
+                <!-- <h3>Banner5</h3>
+                <p>We love Bloomberg 2016</p> -->
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="img-responsive center-block" src="gallery/banner6.jpg" >
+            <div class="carousel-caption">
+                <!-- <h3>Banner6</h3>
+                <p>We love Bloomberg 2016</p> -->
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="img-responsive center-block" src="gallery/banner7.jpg" >
+            <div class="carousel-caption">
+                <!-- <h3>Banner7</h3>
+                <p>We love Bloomberg 2016</p> -->
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="img-responsive center-block" src="gallery/banner8.jpg" >
+            <div class="carousel-caption">
+                <!-- <h3>Banner8</h3>
+                <p>We love Bloomberg 2016</p> -->
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="img-responsive center-block" src="gallery/banner9.jpg" width="720" height="315">
+            <div class="carousel-caption">
+                <!-- <h3>Banner9</h3>
+                <p>We love Bloomberg 2016</p> -->
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="img-responsive center-block" src="gallery/banner10.jpg">
+            <div class="carousel-caption">
+                <!-- <h3>Banner10</h3>
+                <p>We love Bloomberg 2016</p> -->
+            </div>
+        </div>
+    </div>
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
