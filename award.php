471c33130c41afb7a46cbb91c7e7fc4bf8501d73
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-08-24 01:06:25 +0800

    Resolved for award.php

--- award.php
+++ award.php
@@ -1,29 +1,17 @@
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <?php include("header.php") ?>
+        <?php include "header.php"; ?>
         
     </head>
     <body>
-        <!-- PRIMARY NAVIGATION -->
-        <?php 
-            include("nav_structure.php");
-        ?>
-        <!--PRIMARY NAVIGATION ENDS HERE -->
 
-        <!-- CAROUSEL STARTS HERE -->
-        <?php
-            include("carousel.php");
-        ?>
-        <!-- CAROUSEL ENDS HERE -->
-        
+        <?php include "nav_structure.php" ;?>
         <?php 
-            include("social_float.php");
-        ?>
-        <?php 
-            // Awards
-            putenv("LANG=$lang");
-            setlocale(LC_ALL, $lang);
+
+            // putenv("LANG=$lang");
+            // setlocale(LC_ALL, $lang);
 
             $domain = array (
                 'award'
@@ -36,39 +24,41 @@
 
             textdomain($domain[0]); // default domain
         ?>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 col-xs-12">
+        
+        <div id="page-wrapper">
+            <?php include "nav_display.php" ?>
+            <div class="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-xs-12">
 
-                    <h1> <?php echo gettext('Penghargaan'); ?> </h1>
-                    <h3> <?php echo gettext('Tahun'); ?> </h3>
-                    <ol class="breadcrumb">
-                        <li><a class="award_year" href="ajax/Penghargaan/2017.php">2017</a></li>
-                        <li><a class="award_year" href="ajax/Penghargaan/2016.php">2016</a></li>
-                        <li><a class="award_year" href="ajax/Penghargaan/2015.php">2015</a></li>
-                        <li><a class="award_year" href="ajax/Penghargaan/2014.php">2014</a></li>
-                        <li><a class="award_year" href="ajax/Penghargaan/2013.php">2013</a></li>
-                        <li><a class="award_year" href="ajax/Penghargaan/2012.php">2012</a></li>
-                        <li><a class="award_year" href="ajax/Penghargaan/2011.php">2011</a></li>
-                        <li><a class="award_year" href="ajax/Penghargaan/2010.php">2010</a></li>
-                        <li><a class="award_year" href="ajax/Penghargaan/2009.php">2009 - 2005</a></li>
-                    </ol>
-                    <div class="award-list">
-                        <?php include("ajax/Penghargaan/2017.php") ?>
-                       
+                            <h1> <?php echo gettext('Penghargaan'); ?> </h1>
+                            <h3> <?php echo gettext('Tahun'); ?> </h3>
+                            <ol class="breadcrumb">
+                                <li><a class="award_year" href="ajax/Penghargaan/2017.php">2017</a></li>
+                                <li><a class="award_year" href="ajax/Penghargaan/2016.php">2016</a></li>
+                                <li><a class="award_year" href="ajax/Penghargaan/2015.php">2015</a></li>
+                                <li><a class="award_year" href="ajax/Penghargaan/2014.php">2014</a></li>
+                                <li><a class="award_year" href="ajax/Penghargaan/2013.php">2013</a></li>
+                                <li><a class="award_year" href="ajax/Penghargaan/2012.php">2012</a></li>
+                                <li><a class="award_year" href="ajax/Penghargaan/2011.php">2011</a></li>
+                                <li><a class="award_year" href="ajax/Penghargaan/2010.php">2010</a></li>
+                                <li><a class="award_year" href="ajax/Penghargaan/2009.php">2009 - 2005</a></li>
+                            </ol>
+                            <div class="award-list">
+                                <?php include("ajax/Penghargaan/2017.php") ?>
+                            
+                            </div>
+                        </div>
+
                     </div>
                 </div>
 
-                <!-- SIDEBAR STARTS HERE -->
-                <?php include("sidebar.php"); ?>
-                <!-- SIDEBAR ENDS HERE -->
             </div>
         </div>
-        <!-- CONTENT ENDS HERE -->
 
-        <!-- FOOTER STARTS HERE -->
         <?php include("footer.php"); ?>
-        <!-- FOOTER ENDS HERE -->
+
     
     <!-- To load jQuery and Javascript -->
     <?php include("jsload.php"); ?>
