6fb2fe09b78b4d05ae3faa2661cc7c733bd091dd
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-08-24 01:16:39 +0800

    Resolved for vision-and-mission.php

--- vision-and-mission.php
+++ vision-and-mission.php
@@ -1,16 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <?php include("header.php") ?>
+        <?php include "header.php"; ?>
     </head>
     <body>
         <?php
-            include("nav_structure.php");
-            include("carousel.php")
+            include "nav_structure.php";
         ?>
         <?php 
-            putenv("LANG=$lang");
-            setlocale(LC_ALL, $lang);
+            // putenv("LANG=$lang");
+            // setlocale(LC_ALL, $lang);
 
             $domain = array (
                 'vision_mission'
@@ -23,40 +22,38 @@
 
             textdomain($domain[0]); // default domain
         ?>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8 col-xs-12">
-                    <h1><?php echo gettext('Visi dan Misi'); ?></h1>
-                    <h3><?php echo gettext('Visi'); ?></h3>
-                    <p><?php echo gettext('PT Sinarmas Asset Management selalu berinovasi dan mengembangkan produk – produk investasi yang ada guna memberikan hasil yang optimal kepada nasabah, serta dapat disesuaikan dengan kebutuhan nasabah dan calon nasabah baik nasabah individu maupun nasabah korporasi, lembaga keuangan, asuransi dan dana pensiun.'); ?></p>
-                    <p><?php echo gettext('PT Sinarmas Asset Management selalu berusaha untuk menjadi partner yang baik bagi nasabah dan calon nasabahnya dalam berinvestasi dengan terus meningkatkan kualitas dan jangkauan pelayanan melalui seluruh kantor cabang yang ada.'); ?></p>
-                    <h3><?php echo gettext('Misi'); ?></h3>
-                    
+        <div id="page-wrapper">
+            <?php include "nav_display.php" ?>
+            <div class="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-xs-12">
+                            <h1><?php echo gettext('Visi dan Misi'); ?></h1>
+                            <h3><?php echo gettext('Visi'); ?></h3>
+                            <p><?php echo gettext('PT Sinarmas Asset Management selalu berinovasi dan mengembangkan produk – produk investasi yang ada guna memberikan hasil yang optimal kepada nasabah, serta dapat disesuaikan dengan kebutuhan nasabah dan calon nasabah baik nasabah individu maupun nasabah korporasi, lembaga keuangan, asuransi dan dana pensiun.'); ?></p>
+                            <p><?php echo gettext('PT Sinarmas Asset Management selalu berusaha untuk menjadi partner yang baik bagi nasabah dan calon nasabahnya dalam berinvestasi dengan terus meningkatkan kualitas dan jangkauan pelayanan melalui seluruh kantor cabang yang ada.'); ?></p>
+                            <h3><?php echo gettext('Misi'); ?></h3>
+                            
 
-                    <ol>
-                        <li> <?php echo gettext('Memberikan kepercayaan dan kenyamanan bagi nasabah dengan menggunakan sumber daya manusia yang handal dan professional.'); ?></li>
-                        <li><?php echo gettext('Menyediakan beragam produk investasi yang menguntungkan bagi para nasabah dan investor.'); ?> </li>
-                        <li><?php echo gettext('Meningkatkan teknologi informasi dan komunikasi untuk mendukung pelayanan kepada nasabah.'); ?> </li>
-                        <li><?php echo gettext('Melakukan kerjasama dengan mitra usaha dan aliansi bisnis didalam maupun luar negeri.'); ?> </li>
-                    </ol>
-                    
+                            <ol>
+                                <li> <?php echo gettext('Memberikan kepercayaan dan kenyamanan bagi nasabah dengan menggunakan sumber daya manusia yang handal dan professional.'); ?></li>
+                                <li><?php echo gettext('Menyediakan beragam produk investasi yang menguntungkan bagi para nasabah dan investor.'); ?> </li>
+                                <li><?php echo gettext('Meningkatkan teknologi informasi dan komunikasi untuk mendukung pelayanan kepada nasabah.'); ?> </li>
+                                <li><?php echo gettext('Melakukan kerjasama dengan mitra usaha dan aliansi bisnis didalam maupun luar negeri.'); ?> </li>
+                            </ol>
+                            
+                        </div>
+                        
+                    </div>
                 </div>
-                <!-- SIDEBAR STARTS HERE -->
-                <?php 
-                    include("sidebar.php");
-                ?>
-                <!-- SIDEBAR ENDS HERE -->
+            
             </div>
+            <?php include "footer.php"; ?>
         </div>
-        <!-- FOOTER STARTS HERE -->
-        <?php 
-            include("footer.php");
-        ?>
-        <!-- FOOTER ENDS HERE -->
 
-    <?php
-        include("jsload.php");
-    ?>
+        
+
+    <?php include("jsload.php");?>
     </body>
 
 
