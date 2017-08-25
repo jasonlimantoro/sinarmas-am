c826752bff4d2ebf7751d9b993fd791140a3a074
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-08-25 20:38:25 +0800

    Added submenus and their translation in top-navbar

--- nav_display.php
+++ nav_display.php
@@ -32,10 +32,74 @@
             <div class="col-md-8">
                 <div class="topbar-nav">
                     <ul class="nav navbar-nav">
-                        <li><a href="">Produk</a></li>
+                        <li><a href="#"><?php echo dgettext('navigation', 'Produk') ?><span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#"><?php echo dgettext('navigation', 'Reksa Dana Pasar Uang') ?><span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="produk-danamas-rupiah-plus">Danamas Rupiah Plus</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#"><?php echo dgettext('navigation', 'Reksa Dana Pendapatan Tetap') ?><span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="produk-danamas-stabil">Danamas Stabil</a></li>
+                                        <li><a href="produk-simas-danamas-mantap-plus">Simas Danamas Mantap Plus</a></li>
+                                        <li><a href="produk-danamas-dolar">Danamas Dollar</a></li>
+                                        <li><a href="produk-simas-danamas-instrumen-negara">Simas Danamas Instrumen Negara</a></li>
+                                        <li><a href="produk-danamas-pasti">Danamas Pasti</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#"><?php echo dgettext('navigation', 'Reksa Dana Campuran') ?><span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="produk-simas-syariah-berkembang">Simas Syariah Berkembang</a></li>
+                                        <li><a href="produk-simas-satu">Simas Satu</a></li>
+                                        <li><a href="produk-danamas-fleksi">Danamas Fleksi</a></li>
+                                        <li><a href="produk-simas-satu-prima">Simas Satu Prima</a></li>
+                                    </ul>
+                                <li>
+                                <li><a href="#"><?php echo dgettext('navigation', 'Reksa Dana Saham') ?><span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="produk-simas-syariah-unggulan">Simas Syariah Unggulan</a></li>
+                                        <li><a href="produk-simas-danamas-saham">Simas Danamas Saham</a></li>
+                                        <li><a href="produk-simas-saham-unggulan">Simas Saham Unggulan</a></li>
+                                        <li><a href="produk-simas-saham-bertumbuh">Simas Saham Bertumbuh</a></li>
+                                        <li><a href="produk-simas-saham-maksima">Simas Saham Maksima</a></li>
+                                    </ul>
+                                <li>
+                                
+                                <li><a href="#"><?php echo dgettext('navigation', 'Reksa Dana Terproteksi') ?><span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        
+                                        <li class="divider"></li>
+                                        <li class="dropdown-header">Simas Terproteksi</li>
+                                        
+                                        <li><a href="produk-simas-terproteksi-5">Simas Terproteksi 5</a></li>
+                                        <li><a href="produk-simas-terproteksi-6">Simas Terproteksi 6</a></li>
+                                        <li><a href="produk-simas-terproteksi-7">Simas Terproteksi 7</a></li>
+                                        <li><a href="produk-simas-terproteksi-8">Simas Terproteksi 8</a></li>
+                                        <li><a href="produk-simas-terproteksi-9">Simas Terproteksi 9</a></li>
+                                        <li><a href="produk-simas-terproteksi-10">Simas Terproteksi 10</a></li>
+                                        
+                                        <li class="divider"></li>
+                                        <li class="dropdown-header">Simas Gemilang</li>
+
+                                        <li><a href="produk-simas-gemilang-2">Simas Gemilang 2</a></li>
+                                        <li><a href="produk-simas-gemilang-3">Simas Gemilang 3</a></li>
+                                        <li><a href="produk-simas-gemilang-4">Simas Gemilang 4</a></li>
+                                        <li><a href="produk-simas-gemilang-5">Simas Gemilang 5</a></li>
+                                        <li><a href="produk-simas-gemilang-6">Simas Gemilang 6</a></li>
+                                        <li><a href="produk-simas-gemilang-7">Simas Gemilang 7</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
                         <li><a href="">Riset</a></li>
-                        <li><a href="">Unduh</a></li>
-                        <li><a href="">Edukasi</a></li>
+                        <li><a href="#"><?php echo dgettext('navigation', 'Unduh') ?><span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="download-form"><?php echo dgettext('navigation', 'Formulir') ?></a></li>
+                                <li><a href="download-prospectus"><?php echo dgettext('navigation', 'Prospektus') ?> </a></li>
+                            </ul>
+                        </li>
+                        <li><a href=""><?php echo dgettext('navigation', 'Edukasi'); ?></a></li>
                         <li><a href="">FAQ</a></li>
                         <li><a href="?lang=<?php echo dgettext('navigation', 'en_US'); ?>"><?php echo dgettext('navigation', 'English'); ?></a></li>
                     </ul>
