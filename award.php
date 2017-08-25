6b394ab95ed9d2cada621110d6ebe67c5850e37f
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-08-25 16:46:13 +0800

    Resolved translation for award.php

--- award.php
+++ award.php
@@ -9,10 +9,6 @@
 
         <?php include "nav_structure.php" ;?>
         <?php 
-
-            // putenv("LANG=$lang");
-            // setlocale(LC_ALL, $lang);
-
             $domain = array (
                 'award'
             );
