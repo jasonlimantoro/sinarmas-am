d8bcc9ab5027e5acec5224bd3cf2f8174fcf2b06
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-10-08 01:20:15 +0700

    Adding scrollspy and affix index_news

--- index_news.php
+++ index_news.php
@@ -29,54 +29,74 @@
                         <div class="col-md-8 col-xs-12"> 
                             <h1>News</h1>
 
-                            <h3 class="news-title-0"></h3>
+                            <h3 class="news-title-0" id="news-0"></h3>
                             <span class="news-date-0"></span>
                             <p class="news-detail-0"></p>
-                            <h3 class="news-title-1"></h3>
+                            <h3 class="news-title-1" id="news-1"></h3>
                             <span class="news-date-1"></span>
                             <p class="news-detail-1"></p>
-                            <h3 class="news-title-2"></h3>
+                            <h3 class="news-title-2" id="news-2"></h3>
                             <span class="news-date-2"></span>
                             <p class="news-detail-2"></p>
-                            <h3 class="news-title-3"></h3>
+                            <h3 class="news-title-3" id="news-3"></h3>
                             <span class="news-date-3"></span>
                             <p class="news-detail-3"></p>
-                            <h3 class="news-title-4"></h3>
+                            <h3 class="news-title-4" id="news-4"></h3>
                             <span class="news-date-4"></span>
                             <p class="news-detail-4"></p>
-                            <h3 class="news-title-5"></h3>
+                            <h3 class="news-title-5" id="news-5"></h3>
                             <span class="news-date-5"></span>
                             <p class="news-detail-5"></p>
-                            <h3 class="news-title-6"></h3>
+                            <h3 class="news-title-6" id="news-6"></h3>
                             <span class="news-date-6"></span>
                             <p class="news-detail-6"></p>
-                            <h3 class="news-title-7"></h3>
+                            <h3 class="news-title-7" id="news-7"></h3>
                             <span class="news-date-7"></span>
                             <p class="news-detail-7"></p>
-                            <h3 class="news-title-8"></h3>
+                            <h3 class="news-title-8" id="news-8"></h3>
                             <span class="news-date-8"></span>
                             <p class="news-detail-8"></p>
-                            <h3 class="news-title-9"></h3>
+                            <h3 class="news-title-9" id="news-9"></h3>
                             <span class="news-date-9"></span>
                             <p class="news-detail-9"></p>
                             
                         </div>
-                        <div class="col-md-4 col-xs-12">
-                            <h1>Archived</h1>
-                            <ul>
-                                <li><a href="" class="news-date-0"></a></li>
-                                <li><a href="" class="news-date-1"></a></li>
-                                <li><a href="" class="news-date-2"></a></li>
-                                <li><a href="" class="news-date-3"></a></li>
-                                <li><a href="" class="news-date-4"></a></li>
-                                <li><a href="" class="news-date-5"></a></li>
-                                <li><a href="" class="news-date-6"></a></li>
-                                <li><a href="" class="news-date-7"></a></li>
-                                <li><a href="" class="news-date-8"></a></li>
-                                <li><a href="" class="news-date-9"></a></li>
+                        <nav class="col-md-4 col-xs-12 sidebar-archived hidden-xs hidden-sm hidden-print">
+                            <ul id="sidebar-affix" class="nav nav-stacked">
+                                <h1>Archived</h1>
+                                <li>
+                                    <a href="#news-0" class="news-date-0"></a>
+                                </li>
+                                <li>
+                                    <a href="#news-1" class="news-date-1"></a>
+                                </li>
+                                <li>
+                                    <a href="#news-2" class="news-date-2"></a>
+                                </li>
+                                <li>
+                                    <a href="#news-3" class="news-date-3"></a>
+                                </li>
+                                <li>
+                                    <a href="#news-4" class="news-date-4"></a>
+                                </li>
+                                <li>
+                                    <a href="#news-5" class="news-date-5"></a>
+                                </li>
+                                <li>
+                                    <a href="#news-6" class="news-date-6"></a>
+                                </li>
+                                <li>
+                                    <a href="#news-7" class="news-date-7"></a>
+                                </li>
+                                <li>
+                                    <a href="#news-8" class="news-date-8"></a>
+                                </li>
+                                <li>
+                                    <a href="#news-9" class="news-date-9"></a>
+                                </li>
                             </ul>
                         
-                        </div>
+                        </nav>
                     </div>
                 </div>
             </div>
@@ -108,18 +128,25 @@
                         $(".news-title-" + i).append(get_title);
                         $(".news-date-" + i).append(get_date);
                         $(".news-detail-" + i).append(get_detail)
-                        // To center-aligned the title
-                        $(".section1 p.MsoNormal").removeAttr("style");
+                        // To make the table responsive
+                        $("table.MsoNormalTable").removeAttr("style").addClass("table table-responsive");
                     });
 
                     // stop after 10 news!
                     return (i !== 9);
-
                 });
-                
-
             });
-        
+            $('body')
+            .scrollspy({
+                target: '.sidebar-archived',
+                offset: 30
+            });
+            $('#sidebar-affix').affix({
+                offset: {
+                    top: 40,
+                    bottom: 400
+                }
+            });
         </script>
     </body>
 </html>
