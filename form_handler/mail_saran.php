2863a2f3fb53dd6c876b2e79ab773c15c53a9d63
jasonlimantoro <jasonlimantoro99@gmail.com>
2017-10-17 12:24:55 +0800

    fix mail php script

--- form_handler/mail_saran.php
+++ form_handler/mail_saran.php
@@ -5,33 +5,36 @@
         'email',
         'phone',
         'handphone',
-        'kritik'
+        'komentar'
     );
     $error = false;
     foreach ($field as $f) {
         if (!isset($_POST[$f]) || $_POST[$f]==="") {
             $error = true;
-
         }
-
     }
 
     if ($error) {
         echo "<p align='center'>";
-        echo "Tidak ada data tersedia";
-        echo "<br>";
         echo "Masukan Data Dengan benar!";
         echo "</p>";
     }
     else {
         $name = $_POST['name'];
-        $email = $_POST['email'];
         $phone = $_POST['phone'];
         $handphone = $_POST['handphone'];
-        $kritik = $_POST['kritik'];
 
-        $var_array = array($name, $email, $phone, $handphone, $kritik);
+        $to = 'cs@sinarmas-am.co.id';
+        $from = $_POST['email'];
+        $subject = "Comments from " . $name . '(' . $phone . '/' . $handphone . ')';
+        $msg = $_POST['komentar'];
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $headers .= 'From: '. $name . '<'. $from .'>' . "\r\n";
 
+        if (mail($to, $subject, $msg, $headers)) {
+            echo "<h1> Email telah berhasil dikirim ke $to </h1><br>";
+        }
     }
 ?>
 
